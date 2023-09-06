1d25f803cc3b4da4b2daa0beebe1b4da76605441
kcreinawicked <kcreina@me.com>
2023-09-06 21:48:54 +0100

    MediaText and AboutFeature

--- app/components/AboutUsFeature.js
+++ app/components/AboutUsFeature.js
@@ -1,26 +1,69 @@
 "use client";
 import { styled } from "styled-components";
-import Section from "./Section";
 import Image from "next/image";
 import Link from "next/link";
+import AngledArrow from '../../public/images/icons/angledArrow.svg?url';
 
-const CircleContainer = styled.div``;
+const AboutFeatureContainer = styled.section`
+	width: 100%;
+	height: 80vh;
+	position: relative;
+	background: url(${(props) => props.bgimage}) no-repeat center center;
+	background-size: cover !important;
+	-webkit-background-size: cover !important;
+	-moz-background-size: cover !important;
+	-o-background-size: cover !important;
+`;
+
+const CircleContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	top: 0px;
+	z-index: 1;
+	left: 0px;
+	right: 0px;
+	bottom: 0px;
+	background: url(${(props) => props.bgimage}) no-repeat center center;
+	background-size: contain !important;
+	-webkit-background-size: contain !important;
+	-moz-background-size: contain !important;
+	-o-background-size: contain !important;
+`;
+
+const InnerContainer = styled.div`
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	position: absolute;
+	z-index: 2;
+
+	& img.logoMark {
+		display: block;
+		margin: 0 auto 4rem;auto;
+		width: 100%;
+		max-width: calc(100% * 1);
+		height: auto;
+	}
+`;
 
 export default function AboutUsFeature(props) {
 	return (
-		<Section
+		<AboutFeatureContainer
 			anchor="about-us"
 			bgimage={`${props.BGimage.data.attributes.url}`}>
 			<CircleContainer
 				bgimage={`${props.CircleBGimage.data[0].attributes.url}`}>
+				<InnerContainer>
 				<Image
 					src={`${props.Logo.data.attributes.url}`}
 					alt="Circle"
 					width={100}
 					height={100}
+					className="logoMark"
 				/>
-				<Link href={props.CTAurl}>{props.CTAtext}</Link>
+				<Link className="primaryBtnWhite" href={props.CTAurl}><span>{props.CTAtext}</span><Image src={AngledArrow} alt="angled arrow" width={15} height={15}/></Link>
+				</InnerContainer>
 			</CircleContainer>
-		</Section>
+		</AboutFeatureContainer>
 	);
 }
