a76ae3519f53f0ab633c7248017423ac7a0c3fb1
kennethtaylor <taylor.kennethg@gmail.com>
2023-10-23 15:03:03 -0700

    clean up

--- app/reports/[report]/page.js
+++ app/reports/[report]/page.js
@@ -3,14 +3,15 @@ import sectionRenderer from "../../utils/sectionRenderer";
 import getReportBySlug from "../../../lib/getReportBySlug";
 import Hero from "@/app/components/Hero";
 import getPageSections from "@/lib/getPageSections";
+import { DateTime } from "luxon";
 
 export default async function Page({ params }) {
 	const data = await getReportBySlug(params?.report);
 
-	const sections = await getPageSections(data.data[0].id, `reports`);
-	// console.log(data.data[0].id);
-	const title = data.data[0]?.attributes?.Title;
-	const publishedAt = data.data[0]?.attributes?.publishedAt;
+	const sections = await getPageSections(data?.data[0]?.id, `reports`);
+
+	const title = data?.data[0]?.attributes?.Title;
+	const publishedAt = data?.data[0]?.attributes?.publishedAt;
 
 	return (
 		<>
